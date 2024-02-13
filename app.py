38620efcbcef886f8dfdf8648e55dc7331cfe721
AoWangPhilly <aw3338@drexel.edu>
2024-02-13 12:57:25 -0500

    Add database initialization and update about page layout

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from flask import Flask
 from api.home_bp import home_bp
 from api.about_bp import about_bp
 from api.auth_bp import auth_bp
+from database import init_db
 
 app = Flask(__name__)
 
+init_db()
 app.register_blueprint(home_bp, url_prefix="/")
 app.register_blueprint(about_bp, url_prefix="/about")
 app.register_blueprint(auth_bp, url_prefix="/auth")
