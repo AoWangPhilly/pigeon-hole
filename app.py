e895108d4ec6bc2df54e62015d324ba8ad5db787
AoWangPhilly <aw3338@drexel.edu>
2024-02-13 12:20:32 -0500

    Refactor app.py and home.html

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+# app.py
 from flask import Flask
-from api.home import home
+from api.home_bp import home_bp
+from api.about_bp import about_bp
+from api.auth_bp import auth_bp
 
 app = Flask(__name__)
 
-# Register the blueprint with the app
-app.register_blueprint(home)
+app.register_blueprint(home_bp, url_prefix="/")
+app.register_blueprint(about_bp, url_prefix="/about")
+app.register_blueprint(auth_bp, url_prefix="/auth")
 
 if __name__ == "__main__":
     app.run(debug=True)
