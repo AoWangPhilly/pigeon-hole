cc915be89180dfe4320c24f32329027d6e294f91
AoWangPhilly <aw3338@drexel.edu>
2024-02-24 14:18:13 -0500

    add form for adding pigeons

--- api/pigeon_bp.py
+++ api/pigeon_bp.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 pigeon_bp = Blueprint(
     "pigeon",
@@ -9,3 +9,8 @@ pigeon_bp = Blueprint(
 @pigeon_bp.route("/view")
 def view():
     return render_template("view.html")
+
+@pigeon_bp.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "GET":
+        return render_template("add.html")
