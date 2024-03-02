cf7e8b8e00b3aad3f48b9a28a3967133736e7621
Brian Chen <brianchen0810@gmail.com>
2024-03-02 14:19:37 -0500

    Add search functionality to pigeon view page

---
+++ static/js/search.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($) {
+    $("#pigeonSearch").on("keyup", function() {
+        var input = $(this).val();
+
+        $(".card").each(function() {
+            if ($(this).data("name").toUpperCase().indexOf(input) < 0) {
+                $(this).hide();
+            } else {
+                $(this).show();
+            }
+        })
+    });
+});
